da54ed696b5db694e1d7a5db7c52c38551c90966
Vinicius Fantinatto <vifanti@gmail.com>
2020-04-04 14:53:22 -0300

    controle de acesso

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
+import authorizationMiddleware from './app/middlewares/authorization';
 
 const routes = new Router();
 
@@ -12,11 +13,17 @@ routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
+routes.use(authorizationMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/users', UserController.getUser);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
+
+routes.get('/deliverymen', RecipientController.store);
+routes.post('/deliverymen', RecipientController.store);
+routes.put('/deliverymen/:id', RecipientController.store);
+routes.delete('/deliverymen/:id', RecipientController.store);
 
 export default routes;
