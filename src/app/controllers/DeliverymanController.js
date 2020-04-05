416dd9c7cedc8fac2a0ca8abce666e31f66dca43
Vinicius Fantinatto <vifanti@gmail.com>
2020-04-05 02:21:57 -0300

    Upload de arquivos e CRUD de deliveryman

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,95 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliverymanController {
+  async index(req, res) {
+    const deliveryman = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json(deliveryman);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman alredy exists.' });
+    }
+
+    const { id, name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const deliveryman = await Deliveryman.findByPk(id);
+
+    // verifica se o usuário deseja mudar o endereço de email
+    if (email && email !== deliveryman.email) {
+      const userExists = await Deliveryman.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User alredy exists.' });
+      }
+    }
+
+    const { avatar_id } = await deliveryman.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar_id,
+    });
+  }
+
+  async delete(req, res) {
+    await Deliveryman.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.status(204).send();
+  }
+}
+
+export default new DeliverymanController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import File from './app/controllers/FileController';
+import Deliveryman from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 import authorizationMiddleware from './app/middlewares/authorization';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -21,9 +26,11 @@ routes.get('/users', UserController.getUser);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
 
-routes.get('/deliverymen', RecipientController.store);
-routes.post('/deliverymen', RecipientController.store);
-routes.put('/deliverymen/:id', RecipientController.store);
-routes.delete('/deliverymen/:id', RecipientController.store);
+routes.get('/deliverymen', Deliveryman.index);
+routes.post('/deliverymen', Deliveryman.store);
+routes.put('/deliverymen/:id', Deliveryman.update);
+routes.delete('/deliverymen/:id', Deliveryman.delete);
+
+routes.post('/files', upload.single('file'), File.store);
 
 export default routes;
